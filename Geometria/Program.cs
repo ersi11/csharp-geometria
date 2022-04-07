05747ec623700397405ea74ce99ff555c217d907
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-04-07 18:26:13 +0200

    Add project files.

---
+++ Geometria/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//valore base e altezza che l'utente inserirà.
+using Geometria;
+
+Console.WriteLine("inserisci base del rettangolo");
+int baseInputUtente = int.Parse(Console.ReadLine());
+
+Console.WriteLine("inserisci altezza del triangolo");
+int altezzaInputUtente = int.Parse(Console.ReadLine());
+
+rettangolo mioRettangolo = new rettangolo(baseInputUtente, altezzaInputUtente);
+
+mioRettangolo.StampaRettangoloConsole();
---
+++ Geometria/rettangolo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+
+    // ---- classe rettangolo ------
+
+    internal class rettangolo
+    {
+     public int baseRettangolo;
+     public int altezzaRettangolo;
+       
+
+
+    // ------ costruttore -----
+
+        public rettangolo(int baseRettangolo, int altezzaRettangolo)
+        {
+            this.baseRettangolo = baseRettangolo;
+            this.altezzaRettangolo = altezzaRettangolo;
+        }
+
+    //---------- calcolo perimetro del rettangolo ------------
+
+        public int PerimetroRettangolo()
+        {
+            int perimetro = 2 * (baseRettangolo + altezzaRettangolo);
+            return perimetro;
+        }
+
+        //-------- calcolo area del rettangolo ------       
+
+        public int AreaRettangolo()
+        {
+            int area = baseRettangolo * altezzaRettangolo;
+            return area;
+        }
+
+        public void StampaRettangoloConsole()
+        {
+            Console.WriteLine("perimetro è:  " + PerimetroRettangolo());
+            Console.WriteLine("area è:  " + AreaRettangolo());
+        }
+
+        //------- stampare 2 rettangoli eserizio 3 -----
+        
+        public void StampaRettangolo1()
+        {
+          
+
+        }
+
+
+
+
+    }
+}
